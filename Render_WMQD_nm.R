cb7a80c97830063228296c0cb19e1c70bc859335
NMarkgraf <nmarkgraf@hotmail.com>
2023-04-02 16:41:48 +0200

    Zwei kleine Skripte um alles im Hintergrund zu rendern ;-)

---
+++ Render_WMQD_nm.R
@@ -0,0 +1,7 @@
+library(quarto)
+quarto_render("WMQD_01_nm.qmd") # defaults to html
+quarto_render("WMQD_02_nm.qmd") # defaults to html
+quarto_render("WMQD_03_nm.qmd") # defaults to html
+quarto_render("WMQD_04_nm.qmd") # defaults to html
+quarto_render("WMQD_05_nm.qmd") # defaults to html
+quarto_render("WMQD_06_nm.qmd") # defaults to html
---
+++ Render_WM_nm.R
@@ -0,0 +1,5 @@
+library(quarto)
+quarto_render("WM_01_nm.qmd") # defaults to html
+quarto_render("WM_02_nm.qmd") # defaults to html
+quarto_render("WM_03_nm.qmd") # defaults to html
+quarto_render("WM_04_nm.qmd") # defaults to html
