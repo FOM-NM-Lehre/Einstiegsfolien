cbf6c784e2e4952631d4c9fdf7d4e75a7a283601
NMarkgraf <nmarkgraf@hotmail.com>
2023-04-03 14:54:23 +0200

    Anpassung an das neue Layout bis WM_10!

--- Render_WM_nm.R
+++ Render_WM_nm.R
@@ -3,3 +3,9 @@ quarto_render("WM_01_nm.qmd") # defaults to html
 quarto_render("WM_02_nm.qmd") # defaults to html
 quarto_render("WM_03_nm.qmd") # defaults to html
 quarto_render("WM_04_nm.qmd") # defaults to html
+quarto_render("WM_05_nm.qmd") # defaults to html
+quarto_render("WM_06_nm.qmd") # defaults to html
+quarto_render("WM_07_nm.qmd") # defaults to html
+quarto_render("WM_08_nm.qmd") # defaults to html
+quarto_render("WM_09_nm.qmd") # defaults to html
+quarto_render("WM_10_nm.qmd") # defaults to html
